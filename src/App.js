1e49f750c6be58688f145c5a0602fa6a1c1e41e7
Megan Faith Seibert-Hughes <meganfaithhughes@gmail.com>
2023-01-21 14:23:59 -0600

    store site is working so far

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Categories from "./Components/Categories/categories";
+import Header from "./Components/Header/header";
+import Products from "./Components/Products/products";
+
+const App = () => {
+  return (
+    <>
+      <Header></Header>
+      <Categories />
+      <Products />
+    </>
+  );
+};
+
+export default App;
---
+++ src/Components/Footer/footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return(
+        <>
+            <p>&copy2023;Megan Seibert-Hughes</p>
+        </>
+    )
+}
+
+export default Footer;
