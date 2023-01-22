e52e5d23dd5f6424bcb57d96067c268cf32a1ac8
Megan Faith Seibert-Hughes <meganfaithhughes@gmail.com>
2023-01-22 13:29:51 -0600

    add items work with thunk

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,15 @@
 // import reducer from "./reducer";
-import { legacy_createStore as createStore, combineReducers } from "redux";
+import {
+  legacy_createStore as createStore,
+  combineReducers,
+  applyMiddleware,
+} from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import productsReducer from "./products";
 import categoryReducer from "./categories";
 import cartReducer from "./cart";
+// import logger from "./middleware/logger";
+import thunk from "./middleware/thunk";
 
 let reducers = combineReducers({
   products: productsReducer,
@@ -11,6 +17,7 @@ let reducers = combineReducers({
   cart: cartReducer,
 });
 
-const store = () => createStore(reducers, composeWithDevTools());
+const store = () =>
+  createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store();
